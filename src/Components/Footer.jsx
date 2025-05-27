7fe6c4293d0e4fa73de8e31ef8c3b920048b05da
Reignz217 <renzharveysunico17@gmail.com>
2025-05-27 19:17:10 +0800

    footer adjust

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,12 @@
+import {Facebook, Linkedin } from 'lucide-react'
 
 export default function Footer() {
   return (
-    <div className="text-center text-[#00ADB5] text-sm md:py-10 p-5 border-t">
+    <div className="text-center text-[#00ADB5] text-sm md:py-5 py-2 border-t">
+      <div className='flex justify-center gap-5 mb-2'>
+        <a href='https://www.facebook.com/reignz.harvey.9'><Facebook /></a>
+        <a href='https://www.linkedin.com/in/renz-harvey-sunico-76a313356'><Linkedin /></a>
+      </div>
         © {new Date().getFullYear()} Renz Harvey J. Sunico, All rights reserved.
     </div>
   )
