93d5087aa63e157680fca991f866938d156408f9
Reignz217 <renzharveysunico17@gmail.com>
2025-05-28 16:58:47 +0800

    updated resume

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <div className="text-center text-[#00ADB5] text-sm md:py-5 py-2 border-t">
       <div className='flex justify-center gap-5 mb-2'>
-        <a href='https://www.facebook.com/reignz.harvey.9'><Facebook /></a>
-        <a href='https://www.linkedin.com/in/renz-harvey-sunico-76a313356'><Linkedin /></a>
+        <a href='https://www.facebook.com/reignz.harvey.9' aria-label="Visit my Facebook profile" target="_blank" rel="noopener"><Facebook /></a>
+        <a href='https://www.linkedin.com/in/renz-harvey-sunico-76a313356'  aria-label="Visit my LinkedIn profile" target="_blank" rel="noopener"><Linkedin /></a>
       </div>
         © {new Date().getFullYear()} Renz Harvey J. Sunico, All rights reserved.
     </div>
