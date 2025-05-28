69b5b961f16f974ade37fd6c944fc9770298a9fb
Reignz217 <renzharveysunico17@gmail.com>
2025-05-28 17:26:17 +0800

    adjusted image

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
     <section id="about"  className="my-10 p-5 flex flex-col justify-center items-center md:gap-10 gap-2 scroll-mt-20">
         <h4 className="lg:text-5xl md:text-3xl text-xl">About Me</h4>
         <div className="flex lg:flex-row flex-col md:justify-center items-center lg:items-start lg:gap-20 gap-10">
-            <img src="/Profile (1).jpg" alt="Profile Picture" className="lg:w-120 lg:h-120 md:w-90 md:h-90 w-60 aspect-square rounded-xl"></img>
+            <img src="/Profile.webp" alt="Profile Picture" className="lg:w-120 lg:h-120 md:w-90 md:h-90 w-60 aspect-square rounded-xl"></img>
             <div className="flex flex-col lg:gap-5 gap-3">
                 <p className="lg:text-2xl md:text-xl">
                     I'm a front-end web developer focused on building responsive and maintainable websites using React, Tailwind CSS, and JavaScript.
